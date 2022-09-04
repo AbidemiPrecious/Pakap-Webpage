206fbf7186efc22af8bcb92f2358369fcbb2a8b1
Ademola <abidoyepreciousademola@gmail.com>
2022-09-04 16:26:08 +0100

    paka

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ card.addEventListener("mouseover", function() {
  features.classList.add("style");
  // console.log("worked");
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+   document.querySelector(".paka-nav").style.background =  "black"
+   document.querySelector(".navbar-toggler").style.background = "white";
+
+  }
+  else{
+    document.querySelector(".paka-nav").style.background = "transparent";
+    document.querySelector(".navbar-toggler").style.background = "transparent";
+  }
+ }
